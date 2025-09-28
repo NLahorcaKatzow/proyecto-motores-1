29234d5ec234bdb222fa75bf19597b22eb723705
NLahorcaKatzow <nicolas.lahorca.k@gmail.com>
2025-09-27 21:03:56 -0300

    Revert "Merge pull request #7 from NLahorcaKatzow/Feature/LifeSystem"
    This reverts commit 9668f57ab8db6de25fd5e1b64c96cbdaf9a554c6, reversing
    changes made to b43f6b81cd0e107de2c20f0af0386bd20c32be94.

--- Assets/Scripts/Controllers/Character/CharacterControllerNew.cs
+++ Assets/Scripts/Controllers/Character/CharacterControllerNew.cs
@@ -1,16 +1,49 @@
 using UnityEngine;
 
-public class CharacterController : MonoBehaviour
+public class CharacterControllerNew : MonoBehaviour
 {
-    // Start is called once before the first execution of Update after the MonoBehaviour is created
+    public float walkSpeed = 5f;
+    public float runSpeed = 10f;
+    public float jumpHeight = 2f;
+    public float gravity = -9.81f;
+
+    private CharacterController controller;
+    private Vector3 velocity;
+    private bool isGrounded;
+    public LayerMask floorMask;
     void Start()
     {
-        
+        controller = GetComponent<CharacterController>();
     }
 
-    // Update is called once per frame
     void Update()
     {
-        
+        // Verifica si est� tocando el suelo
+        isGrounded = controller.isGrounded;
+        if (isGrounded && velocity.y < 0)
+        {
+            velocity.y = -2f; // Mantiene al jugador en el suelo
+        }
+
+        // Inputs del teclado
+        float horizontal = Input.GetAxis("Horizontal");
+        float vertical = Input.GetAxis("Vertical");
+
+        // Determina si corre o camina
+        float speed = Input.GetKey(KeyCode.LeftShift) ? runSpeed : walkSpeed;
+
+        // Calcula direcci�n de movimiento en base a la orientaci�n del jugador
+        Vector3 move = transform.right * horizontal + transform.forward * vertical;
+        controller.Move(move * speed * Time.deltaTime);
+
+        // Salto
+        if (Input.GetButtonDown("Jump") && isGrounded)
+        {
+            velocity.y = Mathf.Sqrt(jumpHeight * -2f * gravity);
+        }
+
+        // Gravedad
+        velocity.y += gravity * Time.deltaTime;
+        controller.Move(velocity * Time.deltaTime);
     }
 }
---
+++ Assets/Scripts/Controllers/IA/EnemyBase.cs
@@ -0,0 +1,48 @@
+using UnityEngine;
+public class EnemyBase : MonoBehaviour
+{
+    public float health;
+    public float velocity;
+    public float attackDamage;
+    public int attackRange;
+    public float attackCooldown;
+    public GameObject player;
+
+    public void TakeDamage(float damage)
+    {
+        health -= damage;
+        if (health <= 0)
+        {
+            Die();
+        }
+    }
+
+    public virtual void Die()
+    {
+        //TODO: Add die animation
+        
+        Debug.Log("Enemy died");
+        //TODO: Add die sound
+        //TODO: send die event
+        //TODO: destroy enemy
+    }
+
+    public void AttackPlayer()
+    {
+        Debug.Log("Enemy attacked player");
+        //TODO: Add attack animation
+        //TODO: Add attack sound
+        //TODO: send attack damage to player event
+    }
+
+    public void SetPlayer(GameObject player)
+    {
+        this.player = player;
+    }
+
+    public void RemovePlayer()
+    {
+        this.player = null;
+    }
+
+}
